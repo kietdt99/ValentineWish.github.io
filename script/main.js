dba0006188b50eeb1d94e8a0c291de40c9d092cd
Kiet Do <tuankiet141199@gmail.com>
2024-02-14 13:55:18 +0700

    update

--- script/main.js
+++ script/main.js
@@ -252,7 +252,7 @@ const animationTimeline = () => {
     )
     .to(".six", 0.5, {
       opacity: 0,
-      y: 10,
+      y: 30,
       zIndex: "-1",
     })
     .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
